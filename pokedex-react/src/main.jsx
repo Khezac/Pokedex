7b51374a460883260ef385f99823da97bb2ec5f3
Khezac <khezac1@gmail.com>
2024-06-05 17:28:01 -0300

    feat: background da main

--- pokedex-react/src/main.jsx
+++ pokedex-react/src/main.jsx
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Pokedex from './components/pages/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <></>
+    <>
+        <Pokedex/>
+    </>
 )

0c73953a3fa2ef0bb04bb75ddf13939dacf8a5bd
Foncka <matheus_fonseca237@hotmail.com>
2024-06-06 11:50:44 -0300

    feat:Get Pokemon

--- pokedex-react/src/main.jsx
+++ pokedex-react/src/main.jsx
@@ -1,9 +1,18 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Pokedex from './pages/Pokedex/index.jsx'
+import Pokemon from './Services/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
         <Pokedex/>
+        <Pokemon/>
     </>
 )
+
+
+let randomPokemon = Math.floor(Math.random() * 150)
+      console.log(randomPokemon);
+
+      let randomLvl = Math.floor(Math.random() * 70) ;
+      console.log(randomLvl);

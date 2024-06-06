440e8553e4c5a66722bf629e8f6cebbae3aaa5dd
Lucas <amorimrodrigueslucas@gmail.com>
2024-06-06 19:01:25 -0300

    feat: VisorSuperiorDireito

--- pokedex-react/src/components/PokedexDireita/index.jsx
+++ pokedex-react/src/components/PokedexDireita/index.jsx
@@ -1,9 +1,11 @@
 import styles from './style.module.css'
+import { VisorSuperiorDireito } from '../VisorSuperiorDireito';
 
 export const PokedexDireita = () => {
     return (
         <div className={styles.ladoDirContainer}>
             <p>PokedexDireita</p>
+            <VisorSuperiorDireito/>
         </div>
     )
 };
---
+++ pokedex-react/src/components/VisorSuperiorDireito/index.jsx
@@ -0,0 +1,9 @@
+import style from '../VisorSuperiorDireito/style.module.css'
+
+export const VisorSuperiorDireito = () => {
+    return (
+        <div className={style.VisorSuperiorDireito}>
+            <p>Visor superior</p>
+        </div>
+    )
+}

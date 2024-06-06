a0b58e2923f1940fb28c55fcfe4f314695569e51
Khezac <khezac1@gmail.com>
2024-06-06 11:20:16 -0300

    feat: informações do visor inferior esquerdo e refatoração de responsabilidade do pad

--- pokedex-react/src/components/Pad/index.jsx
+++ pokedex-react/src/components/Pad/index.jsx
@@ -3,10 +3,7 @@ import styles from './styles.module.css'
 export const Pad = () => {
 
     function handlePad(){
-        let randomLvl = Math.floor(Math.random() * (20 - 1) + 1)
-        let randomPokemon = Math.floor(Math.random() * 150)
-        console.log(randomPokemon);//Este número será o id para requisição de get na pokeApi
-        console.log(randomLvl);//Este será o level do pokemon que será mandado para o visor de baixo da esquerda e armazenado na API
+      console.log("pegará informações da api")
     }
 
   return (

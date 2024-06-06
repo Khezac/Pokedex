5c20a3572d947806a0055efedadf0ff2744ea2d4
Khezac <khezac1@gmail.com>
2024-06-06 17:11:06 -0300

    fix: alinhamento dos elementos do lado esquerdo

--- pokedex-react/src/pages/Pokedex/index.jsx
+++ pokedex-react/src/pages/Pokedex/index.jsx
@@ -1,17 +1,19 @@
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
-import { Logo } from '../../components/Logo/index.jsx';
-import { PokedexEsquerda } from '../../components/PokedexEsquerda/index.jsx';
-import { PokedexDireita } from '../../components/PokedexDireita/index.jsx';
+import { Logo } from "../../components/Logo/index.jsx";
+import { PokedexEsquerda } from "../../components/PokedexEsquerda/index.jsx";
+import { PokedexDireita } from "../../components/PokedexDireita/index.jsx";
 
 const Pokedex = () => {
-    return (
-        <div className={styles.pokedexContainer}>
-            <Logo/>
-            <PokedexEsquerda/>
-            <PokedexDireita/>
-        </div>
-    )
+  return (
+    <div className={styles.pokedexContainer}>
+      <Logo />
+      <div className={styles.pokedex}>
+        <PokedexEsquerda />
+        <PokedexDireita />
+      </div>
+    </div>
+  );
 };
 
 export default Pokedex;
